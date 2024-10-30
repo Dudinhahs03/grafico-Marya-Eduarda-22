c7bdd2bc345bb19b79ccbf61560e582386297c89
Dudinhahs03 <144931581+Dudinhahs03@users.noreply.github.com>
2024-10-30 08:53:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,33 @@ labels:["Instagram",
  "Telegram",
  "FB Messenger",
  "LINE"
+],
+datasets:[ {
+data:[
+    16.5,
+16.1,
+12.8,
+12.8,
+7.4,
+6.6,
+3.2,
+2.4,
+2.3,
+1.7
+],
+backgroundColor: [
+    "red",
+    "purple",
+    "pink",
+    "yellow",
+    "white",
+    "blue",
+    "grey",
+    "black",
+    "green",
+    "orange",
 ]
-
+} ]
 }
 }
 )
